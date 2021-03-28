ab2c1b34ffb33faec1d63c97a49447f3204db780
johng <jcguest90@gmail.com>
2021-03-28 02:32:31 -0500

    add alert component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,8 +2,9 @@ import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert.js';
+import PropTypes from 'prop-types';
 
-const Register = (props) => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -86,6 +87,10 @@ const Register = (props) => {
       </p>
     </Fragment>
   );
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
 };
 
 export default connect(null, { setAlert })(Register);
